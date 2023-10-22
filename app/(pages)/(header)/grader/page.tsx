0b7a71bad5a6700b810b558d515ca085546497e7
LandonHarter <landonharter@outlook.com>
2023-10-22 10:42:28 -0400

    fix generating state

--- app/(pages)/(header)/grader/page.tsx
+++ app/(pages)/(header)/grader/page.tsx
@@ -215,7 +215,7 @@ export default function GradePage() {
     );
   }
 
-  if (generating) <Loading />;
+  if (generating) return <Loading />;
   return (
     <>
       <BWArtboard className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[-1]" />
