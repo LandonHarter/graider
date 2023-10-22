2261d23af6d58481eb42b59ca4770642d91d9f6b
LandonHarter <landonharter@outlook.com>
2023-10-22 10:55:15 -0400

    log messages for text extraction

--- app/firebase/text.ts
+++ app/firebase/text.ts
@@ -7,17 +7,23 @@ export async function extractText(image: Blob) {
     const storageRef = ref(storage, 'essays/' + generateId());
     await uploadBytes(storageRef, image);
 
+    console.log('uploaded image to storage');
+
     const text = await new Promise((resolve, reject) => {
         const timer = setTimeout(() => {
             reject(null);
+            console.log('text extraction timed out');
         }, 10000);
 
         const unsubscribe = onSnapshot(collection(db, 'image_to_text'), (querySnapshot) => {
             const doc = querySnapshot.docs[0];
+            console.log(querySnapshot);
             if (!doc.exists() || doc.data().text === null) return;
 
             const data = doc.data();
             if (!data) return;
+
+            console.log('text extracted from image');
 
             clearTimeout(timer);
             resolve(doc.data()?.text);
